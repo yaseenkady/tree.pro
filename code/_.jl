33e3ac380b105c0f3e0aa762326c5506e7ab51d7
KwatME <kwat.me@icloud.com>
2021-10-28 14:36:36 -0600

    Refactored

--- code/_.jl
+++ code/_.jl
@@ -13,20 +13,26 @@ SE = Kwat.workflow.read_setting(pas)
 PAR, PAI, PAC, PAO = Kwat.workflow.get_path(pas)
 
 # ==============================================================================
+function shorten_path(pa::String, n_ba::Int64)::String
+
+    return joinpath(splitpath(pa)[(end - n_ba):end]...)
+
+end
+
 function shorten_path(pa::String, di::String)::String
 
     sp_ = splitpath(pa)
 
-    id = findfirst(sp_ .== di)
+    id = findfirst(sp_ .== split(di, "/")[end]) + 1
 
-    return joinpath(sp_[(id + 1):end]...)
+    return shorten_path(pa, length(sp_) - id)
 
 end
 
 function move(
     pa1::String,
     pa2::String;
-    n_di::Int64 = 0,
+    n_ba::Int64 = 1,
     te::Bool = false,
 )::String
 
@@ -34,13 +40,13 @@ function move(
 
     sp2_ = splitpath(pa2)
 
-    n_sk = length(Kwat.vector.get_longest_common_prefix([sp1_, sp2_])) - n_di
+    n_sk = length(Kwat.vector.get_longest_common_prefix([sp1_, sp2_])) - n_ba
 
     println(joinpath(sp1_[n_sk:end]...), " ==> ", joinpath(sp2_[n_sk:end]...))
 
     if te
 
-        return "Testing (not moved)."
+        return ""
 
     else
 
@@ -60,33 +66,27 @@ function walk_up(hi::String)::Vector{Vector{Union{String, Vector{String}}}}
 
 end
 
-function is_good_structure(na::String)::Bool
+function is_good_name(na::String)::Bool
 
     return occursin(r"^([0-9]+\.){1,2}[a-z0-9_]+(\.md$|$)", na)
 
 end
 
-function split_good_structure(na::String)::Tuple{Float64, String, String}
+function split_name(na::String)::Vector{Union{Int64, Float64, String}}
 
     sp_ = string.(split(na, "."))
 
-    if !endswith(na, ".md")
+    it1 = parse(Int64, sp_[1])
 
-        sp_ = push!(sp_, "/")
+    it2 = tryparse(Int64, sp_[2])
 
-    end
+    if isnothing(it2)
 
-    fl1 = parse(Float64, sp_[1])
-
-    fl2 = tryparse(Float64, sp_[2])
-
-    if isnothing(fl2)
-
-        pa_ = (fl1, sp_[2:3]...)
+        pa_ = [it1; sp_[2:end]]
 
     else
 
-        pa_ = (fl1 + fl2 / 10.0, sp_[3:4]...)
+        pa_ = [it1 + it2 / 10; sp_[3:end]]
 
     end
 
@@ -94,13 +94,85 @@ function split_good_structure(na::String)::Tuple{Float64, String, String}
 
 end
 
-function make_id(pa::String, hi::String)::Vector{Int64}
+function make_continuous(
+    ro::String,
+    na_::Vector{String};
+    te::Bool = false,
+)::Nothing
+
+    for (id, na) in enumerate(sort!(na_; by = split_name))
+
+        co = join((id, split_name(na)[2:end]...), ".")
+
+        if na != co
+
+            move(joinpath(ro, na), joinpath(ro, co); te = te)
+
+        end
+
+    end
+
+    return nothing
+
+end
+
+function id_name(pa::String, hi::String)::Vector{Int64}
 
     sp_ = splitpath(pa)
 
     return [
         parse(Int64, split(sp, ".")[1]) for
-        sp in sp_[(findfirst(sp_ .== hi) + 1):end]
+        sp in sp_[(findfirst(sp_ .== split(hi, "/")[end]) + 1):end]
     ]
+
+end
+
+function get_parent_title(ro::String)::String
+
+    return split_name(splitdir(ro)[2])[2]
+
+end
+
+function catalog(hi)::Dict{String, Dict{String, Union{String, Vector{Int64}}}}
+
+    ti_di = Dict{String, Dict{String, Union{String, Vector{Int64}}}}()
+
+    for (ro, di_, fi_) in walk_up(hi)
+
+        for fi in fi_
+
+            ti = split_name(fi)[2]
+
+            if ti == "_"
+
+                ti = get_parent_title(ro)
+
+            end
+
+            pa = joinpath(ro, fi)
+
+            if haskey(ti_di, ti)
+
+                error(
+                    "2: ",
+                    shorten_path(ti_di[ti]["path"], 1),
+                    " and ",
+                    shorten_path(pa, 1),
+                )
+
+            else
+
+                ti_di[ti] = Dict(
+                    "path" => pa,
+                    "id" => id_name(pa, string(rstrip(hi, '/'))),
+                )
+
+            end
+
+        end
+
+    end
+
+    return ti_di
 
 end
